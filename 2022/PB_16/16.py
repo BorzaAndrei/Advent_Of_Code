3974341b2659ec98022c4c138ef7528622663d79
BorzaAndrei <andreiborza98@gmail.com>
2022-12-18 21:26:36 +0200

    Day 16 - working

--- 2022/PB_16/16.py
+++ 2022/PB_16/16.py
@@ -1,6 +1,9 @@
 from __future__ import annotations
+
+import math
 from dataclasses import dataclass
 import re
+import itertools
 
 
 @dataclass
@@ -9,63 +12,6 @@ class ValveRoom:
     flow: int
     leads_to = []
     leads_to_names = []
-    current_flow = 0
-
-    def paths_available(self, visited):
-        for vr in self.leads_to:
-            if (self.name, vr.name) not in visited and (vr.name, self.name) not in visited:
-                return True
-        return False
-
-    def calculate_max_score(self, score, time, visited, skipped, all_valves, came_from=None):
-        any_open = False
-        for v in all_valves:
-            if v.current_flow == v.flow:
-                any_open = True
-
-        changed_here = False
-        if self.current_flow > 0 or not any_open and time > 0:
-            changed_here = True
-            time -= 1
-            score += self.flow * time
-            self.current_flow = 0
-
-        max_score = 0
-        max_path = []
-        no_path_available = True
-        if time != 0:
-            for vr in self.leads_to:
-                if (came_from is None or came_from != vr.name or not vr.paths_available(visited)) and (
-                        (self.name, vr.name) not in visited or vr.paths_available(visited)):
-                    no_path_available = False
-                    s, path, skipped = vr.calculate_max_score(score, time - 1, visited + [(self.name, vr.name)],
-                                                              skipped, all_valves, self.name)
-                    if changed_here:
-                        score_without_opening_this, path_without_opening_this, skipped = vr.calculate_max_score(
-                            score - self.flow * time, time, visited + [(self.name, vr.name)], skipped, all_valves,
-                            self.name)
-                        if score_without_opening_this > s:
-                            skipped.add(self.name)
-                            s, path = score_without_opening_this, path_without_opening_this
-                    if s > max_score:
-                        max_score = s
-                        max_path = path
-        if time == 0 or no_path_available:
-            max_path = visited
-            max_score = score
-        if changed_here:
-            self.current_flow = self.flow
-
-        # any_open = False
-        # for v in all_valves:
-        #     if v.current_flow == v.flow:
-        #         any_open = True
-        # 
-        # # Pickup stragglers
-        # if any_open and len(skipped) > 0:
-        #     for v in self.leads_to:
-
-        return max_score, max_path, skipped
 
     def __str__(self):
         return f"Valve {self.name} - Rate={self.flow} - Leads To: {','.join([v.name for v in self.leads_to])}"
@@ -83,8 +29,55 @@ with open("input.txt") as file:
         v.current_flow = v.flow
         valves.append(v)
 
+non_empty_valves = set()
 for v in valves:
     for vlt in v.leads_to_names:
         v.leads_to.append([valve for valve in valves if valve.name == vlt][0])
+    if v.flow > 0:
+        non_empty_valves.add(v.name)
 
-print(valves[0].calculate_max_score(0, 30, [], set(), valves))
+dist = {v.name: {vlt.name: math.inf for vlt in valves} for v in valves}
+nxt = {v.name: {vlt.name: None for vlt in valves} for v in valves}
+for v in valves:
+    dist[v.name][v.name] = 0
+    nxt[v.name][v.name] = v
+    for vlt in v.leads_to:
+        dist[v.name][vlt.name] = 1
+        nxt[v.name][vlt.name] = vlt
+for k in range(len(valves)):
+    for i in range(len(valves)):
+        for j in range(len(valves)):
+            vlv_i = valves[i].name
+            vlv_j = valves[j].name
+            vlv_k = valves[k].name
+            if dist[vlv_i][vlv_j] > dist[vlv_i][vlv_k] + dist[vlv_k][vlv_j]:
+                dist[vlv_i][vlv_j] = dist[vlv_i][vlv_k] + dist[vlv_k][vlv_j]
+                nxt[vlv_i][vlv_j] = nxt[vlv_i][vlv_k]
+
+
+def calculate_score(all_valves, distances, start, score, time_left, opened, non_empty):
+    if len(non_empty - opened) == 0 or time_left == 0:
+        return score
+    max_score = 0
+    for v_name in non_empty - opened:
+        if time_left - distances[start.name][v_name] - 1 >= 0:
+            opened.add(v_name)
+            v = [x for x in all_valves if x.name == v_name][0]
+            s = calculate_score(all_valves, distances, v,
+                                score + (time_left - distances[start.name][v_name] - 1) * v.flow,
+                                time_left - distances[start.name][v_name] - 1, opened, non_empty)
+            opened.remove(v_name)
+            if s > max_score:
+                max_score = s
+    return max(score, max_score)
+
+
+max_global = 0
+for comb in itertools.combinations(non_empty_valves, len(non_empty_valves) // 2):
+    combinations = set(comb)
+    rest = non_empty_valves - combinations
+    score_me = calculate_score(valves, dist, [x for x in valves if x.name == "AA"][0], 0, 26, set(), combinations)
+    score_elephant = calculate_score(valves, dist, [x for x in valves if x.name == "AA"][0], 0, 26, set(), rest)
+    if score_me + score_elephant > max_global:
+        max_global = score_me + score_elephant
+print(max_global)
